0667fbfd2e9335f0cc5834aeca4841edb2820535
Mikaela Johansson <mikaela@Mikaelas-MacBook-Pro.local>
2023-12-14 15:55:58 +0100

    first commit

---
+++ src/webparts/birthdaysCoderight/components/IBirthdaysCoderightProps.ts
@@ -0,0 +1,7 @@
+export interface IBirthdaysCoderightProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
